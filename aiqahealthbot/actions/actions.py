70a536f5f9167e2c49890ee68e1dbd7816982239
Ankit Patel <ankitpatel.akp4@gmail.com>
2024-05-31 07:08:59 +0530

    git cleaned

--- aiqahealthbot/actions/actions.py
+++ aiqahealthbot/actions/actions.py
@@ -1,16 +1,7 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
+from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 from datetime import datetime
 import logging
 
@@ -34,3 +25,31 @@ class ActionGreet(Action):
         dispatcher.utter_message(text=f"{greeting}! Welcome to aiqahealth Bot. How can I assist you today? Whether you need help with booking an appointment, finding a doctor, or have a health-related question, I'm here to help.")
 
         return []
+    
+class ActionCheckAvailability(Action):
+
+    def name(self) -> Text:
+        return "action_doctor_check_availability"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        doctor = tracker.get_slot("doctor")
+        time = tracker.get_slot("time")
+
+        # Replace this with your actual logic to check the doctor's availability
+        availability = {
+            "Dr. Smith": ["Friday", "10:00 AM"],
+            "Dr. Johnson": ["next Monday morning", "10:00 AM"],
+            "Dr. Green": ["tomorrow afternoon"],
+            # Add the rest of the doctors and their available times
+        }
+
+        if doctor in availability and time in availability[doctor]:
+            dispatcher.utter_message(text=f"{doctor} is available at {time}.")
+            return [SlotSet("time", time), SlotSet("doctor", doctor)]
+        else:
+            dispatcher.utter_message(template="utter_ask_alternative_time")
+            return [SlotSet("time", None)]
+
